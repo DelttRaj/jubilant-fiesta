acb33124e0b47d8b4ed971282d73e4c47eab6494
RDevRoy <160475108+RDevRoy@users.noreply.github.com>
2024-03-25 19:31:42 +0530

    adding facts component

---
+++ blocks/facts/Facts.vue
@@ -0,0 +1,16 @@
+<template>
+  <h4 v-for="fact in facts">{{ fact }}</h4>
+</template>
+<script>
+import { toRefs } from "vue";
+export default {
+  name: "Facts",
+  props: {
+    factProp: Object,
+  },
+  setup(props) {
+    const { factProp } = toRefs(props);
+    return { facts: factProp.value.facts };
+  },
+};
+</script>
